5880d32797dbad3a10c2fb3f38c60e698b3ceb38
rwill <rwill@William>
2023-10-23 07:28:53 +0800

    refactored Main.java to Runner.java

---
+++ src/main/Runner.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author rwill
+ */
+public class Runner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        screens.Main reservations = new screens.Main();
+        
+        reservations.setVisible(true);
+    }
+    
+}
