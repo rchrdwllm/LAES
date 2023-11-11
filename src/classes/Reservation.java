84db43d81069927e6ae77db83086c56612eefab8
William <William@Revision-PC>
2023-11-11 22:55:03 +0800

    updated Reservation class fields

--- src/classes/Reservation.java
+++ src/classes/Reservation.java
@@ -4,6 +4,11 @@
  */
 package classes;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.UUID;
+
 /**
  *
  * @author rwill
@@ -12,16 +17,36 @@ public class Reservation {
     public String name;
     public String contactNumber;
     public String date;
-    public String service;
+    public String typeOfService;
     public String modeOfPayment;
     public String reservationId;
     
-    public Reservation(String name, String contactNumber, String date, String service, String modeOfPayment, String reservationId) {
+    public Reservation(String name, String contactNumber, LocalDate date, String typeOfService, String modeOfPayment) {
         this.name = name;
         this.contactNumber = contactNumber;
-        this.date = date;
-        this.service = service;
+        this.date = date.toString();
+        this.typeOfService = typeOfService;
         this.modeOfPayment = modeOfPayment;
-        this.reservationId = reservationId;
+        this.reservationId = UUID.randomUUID().toString();
+    }
+    
+    public void add() {
+        try {
+            String st = "INSERT INTO reservations (name, contactNumber, date, typeOfService, modeOfPayment, reservationId) VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = Database.sqlConnection.prepareStatement(st);
+            
+            pstmt.setString(1, this.name);
+            pstmt.setString(2, this.contactNumber);
+            pstmt.setString(3, this.date);
+            pstmt.setString(4, this.typeOfService);
+            pstmt.setString(5, this.modeOfPayment);
+            pstmt.setString(6, this.reservationId);
+            
+            pstmt.executeUpdate();
+            
+            System.out.println("Reservation with reservationId " + reservationId + " added successfully");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 }
